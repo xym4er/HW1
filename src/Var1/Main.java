177814acdb3d6e02448bdbd625216df34b78db5b
xym4er <xym4er@gmail.com>
2015-09-21 15:58:42 +0300

    Hello

---
+++ src/Var1/Main.java
@@ -0,0 +1,7 @@
+package Var1;
+
+/**
+ * Created by ChornyiUA on 21.09.2015.
+ */
+public class Main {
+}
---
+++ src/Var2/Main.java
@@ -0,0 +1,10 @@
+package Var2;
+
+/**
+ * Created by ChornyiUA on 21.09.2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
